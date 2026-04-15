0cdd3032a05e904894c89f2233c9a101b2697d53
William Lesniak <william.lesniak@nysed.gov>
2026-04-15 10:16:10 -0400

    initial commit

---
+++ src/main/java/gov/nysed/docker_tutorial/DockerTutorialApplication.java
@@ -0,0 +1,13 @@
+package gov.nysed.docker_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/nysed/docker_tutorial/DockerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.nysed.docker_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
