e0cab198c95ad906723512d2c403e5afc6d08884
William Lesniak <william.lesniak@nysed.gov>
2026-07-15 08:49:21 -0400

    fixing comment

--- src/main/java/gov/nysed/dockertutorial/projection/UserProjection.java
+++ src/main/java/gov/nysed/dockertutorial/projection/UserProjection.java
@@ -1,7 +1,9 @@
 package gov.nysed.dockertutorial.projection;
 
 // when returning from a native query we need a projection to map the DTO to
-// there's an alternate solution with jdbcTemplate mapping but
+// there's a possible alternate solution with jdbcTemplate mapping but it clutters our
+// UserDto object while also requiring us to create a custom user repository
+// which seems like a lot of extra code for no benefit
 public interface UserProjection {
   Long getId();
   String getUserName();
